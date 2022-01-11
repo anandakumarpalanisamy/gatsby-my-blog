3671b7262526d265b51c64729771e55d1c5e7e57
Anandakumar Palanisamy <ananda11k@gmail.com>
2022-01-11 14:01:49 +0000

    adding all our new blog code for gatsby setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,49 @@
-exports.createPages = async ({ actions }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
+  const dsg = createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
-  })
+  });
+
+  const posts = graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }`).then(result => {
+      result.data.allMarkdownRemark.edges.forEach(({node}) => {
+        createPage({
+          path: node.fields.slug,
+          component: require.resolve("./src/templates/blog-post.js"),
+          context: {
+            slug: node.fields.slug
+          }
+        })
+      })
+    });
+
+  return Promise.all([dsg, posts]);
+}
+
+
+exports.onCreateNode = async({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({ node, getNode });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    })
+  }
 }
---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+    <h1>HELLO FROM THE 3rd PAGE</h1>
+  </Layout>
+)
+
+export default SecondPage
